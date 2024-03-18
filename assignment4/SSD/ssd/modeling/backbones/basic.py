ebba64bc6348925f8716b572bd8088872d5b3034
EirikStudent <eirikgha@stud.ntnu.no>
2024-03-18 13:56:34 +0100

    last from me

--- assignment4/SSD/ssd/modeling/backbones/basic.py
+++ assignment4/SSD/ssd/modeling/backbones/basic.py
@@ -27,55 +27,55 @@ class BasicModel(torch.nn.Module):
         pool_stride = 2
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(in_channels=image_channels, output_channels=32, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=image_channels, out_channels=32, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=pool_kernel_size, stride=pool_stride),
-            nn.Conv2d(in_channels=32, output_channels=64, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
             nn.MaxPool2d(kernel_size=pool_kernel_size, stride=pool_stride),
-            nn.Conv2d(in_channels=64, output_channels=64, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
-            nn.Conv2d(in_channels=64, output_channels=self.out_channels[0], kernel_size=kernel_size, stride=2, padding=padding),
+            nn.Conv2d(in_channels=64, out_channels=self.out_channels[0], kernel_size=kernel_size, stride=2, padding=padding),
             nn.ReLU(),
         )
 
         self.feature_extractor_2 = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(in_channels=self.out_channels[0], output_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=self.out_channels[0], out_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, output_channels=self.out_channels[1], kernel_size=kernel_size, stride=2, padding=padding),
+            nn.Conv2d(in_channels=128, out_channels=self.out_channels[1], kernel_size=kernel_size, stride=2, padding=padding),
             nn.ReLU(),
         )
 
         self.feature_extractor_3 = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(in_channels=self.out_channels[1], output_channels=256, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=self.out_channels[1], out_channels=256, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
-            nn.Conv2d(in_channels=256, output_channels=self.out_channels[2], kernel_size=kernel_size, stride=2, padding=padding),
+            nn.Conv2d(in_channels=256, out_channels=self.out_channels[2], kernel_size=kernel_size, stride=2, padding=padding),
             nn.ReLU(),
         )
 
         self.feature_extractor_4 = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(in_channels=self.out_channels[2], output_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=self.out_channels[2], out_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, output_channels=self.out_channels[3], kernel_size=kernel_size, stride=2, padding=padding),
+            nn.Conv2d(in_channels=128, out_channels=self.out_channels[3], kernel_size=kernel_size, stride=2, padding=padding),
             nn.ReLU(),
         )
 
         self.feature_extractor_5 = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(in_channels=self.out_channels[3], output_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=self.out_channels[3], out_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, output_channels=self.out_channels[4], kernel_size=kernel_size, stride=2, padding=padding),
+            nn.Conv2d(in_channels=128, out_channels=self.out_channels[4], kernel_size=kernel_size, stride=2, padding=padding),
             nn.ReLU(),
         )
 
         self.feature_extractor_6 = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(in_channels=self.out_channels[4], output_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
+            nn.Conv2d(in_channels=self.out_channels[4], out_channels=128, kernel_size=kernel_size, stride=1, padding=padding),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, output_channels=self.out_channels[5], kernel_size=kernel_size, stride=1, padding=0),
+            nn.Conv2d(in_channels=128, out_channels=self.out_channels[5], kernel_size=kernel_size, stride=1, padding=0),
             nn.ReLU(),
         )
 
---
+++ todo.md
@@ -0,0 +1,10 @@
+TODO:
+
+- Data augmentation: Forsiktig, men mulig zoom in og ut. Kanskje mye arbeid
+- Kernel size: Tror ikke det er nødvendig å endre, mulig mindre og mindre
+- Mulig gjøre modell mer complex, øke filter size eller flere lag
+- Mulig batch norm
+- Mulig noe regularization, dropout, L2
+- Trikse med aktivteringsfunksjoner
+- Ensemble methods
+- Dataset: Max størrelse er 100pixler og minimum er 15pixler. Mellom 1 og 20 tall per bilde.
